73b7236642e419a0e5655b87f4bd3894e1a59bef
kimsaebyeok <kimseabyek@gmail.com>
2024-03-30 02:27:08 +0900

    프로젝트 최초커밋

---
+++ membership-service/src/main/java/org/fastcampuspay/membership/MembershjpApplication.java
@@ -0,0 +1,12 @@
+package org.fastcampuspay.membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershjpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershjpApplication.class, args);
+    }
+}
